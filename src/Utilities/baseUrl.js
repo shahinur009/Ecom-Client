d34d0de40a0cb74fc50e70b51e3c1132d12be1f5
BP Devs <bpdevs1037@gmail.com>
2025-10-18 22:33:32 +0600

    updated

--- src/Utilities/baseUrl.js
+++ src/Utilities/baseUrl.js
@@ -1,7 +1,7 @@
 //  Development base URL
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://rabsbabyzones.com" 
-    : "https://ecom-server-iota.vercel.app";
+    ? "https://ecom-server-iota.vercel.app"
+    : "http://localhost:5000";
 
 export default baseUrl;
